5677628a8dce7d4c433e316dc79a1818d10b07a6
BentoOre0 <108052776+BentoOre0@users.noreply.github.com>
2026-07-26 21:39:08 +0800

    Create main.cpp

---
+++ TemplatesEtc/main.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using pii = pair<int, int>;
+using pll = pair<long long, long long>;
+const ll INF = numeric_limits<ll>::max();
+const int inf = numeric_limits<int>::max();
+const char nl = '\n', sp = ' ';
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+}
